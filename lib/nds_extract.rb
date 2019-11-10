1fffe37e39fb9b6a0cb75ef1fe9bc8f0b0029d78
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-11-10 06:00:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,5 +11,8 @@ def directors_totals(nds)
       
       while movie_index < nds[dir_index][:movies].length do 
         result[dir_name] =+ nds[dir_index][:movies][movie_index][:worldwide_gross]
+        
+        movie_index += 1 
+      end 
   result 
 end
