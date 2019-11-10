d43285d272f820b02f12d94a8530ccd09f7e025e
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-11-10 05:56:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,8 +2,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  dir_index
+  dir_index = 0 
   result = {}
-  
+    while dir_index < nds.length do 
+      
   result 
 end
